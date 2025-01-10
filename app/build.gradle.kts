ba813e87e642213a7d6e014a38799a92e8edc3ea
ObiTobi64 <obitoTCP@gmail.com>
2025-01-10 00:34:30 -0400

    Crear interfaz de usuario apartado principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.videollamada_llamadavoz_kotlin"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.videollamada_llamadavoz_kotlin"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures{
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/example/videollamada_llamadavoz_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/videollamada_llamadavoz_kotlin/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.videollamada_llamadavoz_kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.videollamada_llamadavoz_kotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
     }
 }
