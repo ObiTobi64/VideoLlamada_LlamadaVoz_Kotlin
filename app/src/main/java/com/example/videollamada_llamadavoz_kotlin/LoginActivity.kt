bd61694ef9f6332696bd3244cc6a55ee64e38590
ObiTobi64 <obitoTCP@gmail.com>
2025-01-13 00:40:48 -0400

    Fin del proyecto

--- app/src/main/java/com/example/videollamada_llamadavoz_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/videollamada_llamadavoz_kotlin/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.example.videollamada_llamadavoz_kotlin
 
+import android.app.Application
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.videollamada_llamadavoz_kotlin.databinding.ActivityLoginBinding
+import com.zegocloud.uikit.prebuilt.call.ZegoUIKitPrebuiltCallConfig
+import com.zegocloud.uikit.prebuilt.call.ZegoUIKitPrebuiltCallService
+import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationConfig
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,7 +20,31 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener{
-            startActivity(Intent(applicationContext, MainActivity::class.java))
+            val myUserId = binding.userId.text.toString()
+            if (myUserId.isNotEmpty()){
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                intent.putExtra("userId",myUserId)
+                startActivity(intent)
+
+                setupZegoUIKit(myUserId)
+            }else{
+                Toast.makeText(application, "Ingresa un nombre de usuario", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun setupZegoUIKit(userId:String){
+        val application : Application  = application
+        val appID : Long = 328023178
+        val appSign : String = "9ef134d250bfa1c4d4a329bce8570b37a6bcab86faee0a97196c1cb4eee416ab"
+        val userName :String = userId
+
+        val callInvitationConfig = ZegoUIKitPrebuiltCallInvitationConfig()
+        ZegoUIKitPrebuiltCallService.init(application,appID,appSign,userId,userName,callInvitationConfig)
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        ZegoUIKitPrebuiltCallService.unInit()
     }
 }
--- app/src/main/java/com/example/videollamada_llamadavoz_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/videollamada_llamadavoz_kotlin/MainActivity.kt
@@ -1,12 +1,65 @@
 package com.example.videollamada_llamadavoz_kotlin
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.videollamada_llamadavoz_kotlin.databinding.ActivityMainBinding
+import com.zegocloud.uikit.service.defines.ZegoUIKitUser
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myUserId = intent.getStringExtra("userId")
+
+        binding.tvNameUserId.text = "Hola $myUserId! \n ¿A quien deseas llamar?"
+
+        binding.etNameUserId.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val userName = binding.etNameUserId.text.toString()
+                if (userName.isNotEmpty()){
+                    startVideoLlamada(userName)
+                    startLlamadaVoz(userName)
+                }else{
+                    Toast.makeText(application,"Ingrese un nombre de usuario", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
+
+    private fun startVideoLlamada(userId:String){
+        val userName : String = userId
+
+        binding.btnVideoLlamada.setIsVideoCall(true)
+        binding.btnVideoLlamada.resourceID = "zego_uikit_call"
+        binding.btnVideoLlamada.setInvitees(listOf(ZegoUIKitUser(userId,userName)))
+    }
+
+    private fun startLlamadaVoz(userId: String){
+
+        val userName : String = userId
+
+        binding.btnVideoLlamadaVoz.setIsVideoCall(false)
+        binding.btnVideoLlamadaVoz.resourceID = "zego_uikit_call"
+        binding.btnVideoLlamadaVoz.setInvitees(listOf(ZegoUIKitUser(userId,userName)))
+
+    }
+
+
 }
